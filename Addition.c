1c989cb0771ee6615e61124acc2f11649f5ad364
AKASH MANE <108176385+AkashMane08@users.noreply.github.com>
2022-06-25 23:35:12 +0530

    Does Addition
    Performs Addition of Integers.

---
+++ Addition.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+//////////////////////////////////////////////////////////
+// 
+// Function Name:	Addition
+// Descreption:	    used to perform addition of two function
+// Input:		Integer, Integer
+// Output:		Integer
+// Date:		26/06/2022
+// Author:		Akash
+//
+//////////////////////////////////////////////////////////
+int Addition(int iValue1, int iValue2)
+{
+	int iAns = 0;
+	iAns = iValue1 + iValue2;
+	return iAns;
+}
+
+//////////////////////////////////////////////////////////
+//Write A program to perform Addition of two function
+//////////////////////////////////////////////////////////
+
+int main ()
+{
+	int iNo1 = 0;
+	int  iNo2 = 0;
+	int iRet = 0;
+	
+	printf("Enter first number: \n");
+	scanf("%d", &iNo1);
+	
+	printf("Enter second number: \n");
+	scanf("%d", &iNo2);
+	
+	iRet = Addition(iNo1, iNo2);
+	
+	printf("Addition is : %d\n", iRet );
+	
+	return 0;
+}
+
+//////////////////////////////////////////////////////////
+//
+// Input: 10, 20
+// Output: 30
+//
+//////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
